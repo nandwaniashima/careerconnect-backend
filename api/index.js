d100a19b3f3509ee069cdcbd637717a607c3a02d
nandwaniashima <ashimanandwani5@gmail.com>
2025-05-22 23:48:03 +0530

    Fix for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -49,6 +49,10 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
 
 const server = http.createServer(app);
 server.timeout = 300000; 
